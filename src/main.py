b48be78535fdda58730e5f4714dc94a433952073
Muhammad Aditya Hilmy <didithilmy@gmail.com>
2023-07-12 16:51:53 +1000

    Add error handling, bootstrap UI

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 import os
-from fastapi import FastAPI, UploadFile, Request
+from fastapi import FastAPI, UploadFile, Request, HTTPException
 from fastapi.responses import StreamingResponse
 from fastapi.templating import Jinja2Templates
 from prediction import ModelPrediction
@@ -9,11 +9,16 @@ templates = Jinja2Templates(directory=os.path.join(os.path.dirname(__file__), 't
 
 @app.get("/")
 def read_root(request: Request):
-    return templates.TemplateResponse("index.html", {'request': request})
+    predict_url = request.url_for('predict_input_file')
+    return templates.TemplateResponse("index.html", {'request': request, 'predict_url': predict_url})
 
 @app.post("/predict")
 def predict_input_file(file: UploadFile):
-    output = ModelPrediction().predict(file.file)
-    response = StreamingResponse(iter([output.getvalue()]), media_type="text/csv")
-    response.headers["Content-Disposition"] = "attachment; filename=output.csv"
-    return response
+    try:
+        output = ModelPrediction().predict(file.file)
+        response = StreamingResponse(iter([output.getvalue()]), media_type="text/csv")
+        response.headers["Content-Disposition"] = "attachment; filename=output.csv"
+        return response
+    except:
+        raise HTTPException(status_code=400, detail="Invalid input file")
+
